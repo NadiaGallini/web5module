acf6386a469b854e97ae6f43a05e28938586b0ee
NadiaGallini <gallini.nadi@yandex.ru>
2024-07-19 14:29:23 +0300

    Исправила 16.2

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-// server.js
 const http = require('http');
 const { fetchDataAndSaveToFile } = require('./getDataAndSaveToFile'); // Импортируем функцию из предыдущего файла
 
 const server = http.createServer((req, res) => {
-    // Произвольный маршрут, например /fetchData
     if (req.url === '/fetchData') {
         const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
         const fileName = 'posts_from_server.json';
 
         fetchDataAndSaveToFile(apiUrl, fileName); // Вызываем функцию для получения данных и сохранения в файл
 
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' }); // Установите кодировку UTF-8
         res.end(`Данные из ${apiUrl} будут сохранены в файл ${fileName}`);
+    } else if (req.url === '/') {
+        // Обработчик для корневого маршрута
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Добро пожаловать на главную страницу!');
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('Маршрут не найден');
     }
 });
